e7c0fdc4a4d920535d3944f10089d3348609e9b9
Sakkris <cristovaogmsampaio0@gmail.com>
2023-03-21 15:24:27 +0000

    Adicionei uma base para o slider da munição.

---
+++ scenes/ui/bullet_slider.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+
+func _ready():
+	# Clear the viewport.
+	var viewport = $SubViewport
+	$SubViewport.set_clear_mode(SubViewport.CLEAR_MODE_ONCE)
+
+	# Retrieve the texture and set it to the viewport quad.
+	$ViewportQuad.material_override.albedo_texture = viewport.get_texture()
