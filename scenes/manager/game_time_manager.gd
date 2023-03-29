0d4787bb774215d21c7521ce1a385b3a74804142
Sakkris <cristovaogmsampaio0@gmail.com>
2023-03-29 16:04:09 +0100

    melhorei o UI do jogador

--- scenes/manager/game_time_manager.gd
+++ scenes/manager/game_time_manager.gd
@@ -9,6 +9,7 @@ var time_checkpoint_rechead = 0
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+	GameEvents.enemy_died.connect(func(): add_time())
 
 
 func _process(delta):
@@ -16,7 +17,16 @@ func _process(delta):
 	
 	if last_time != int(elapsed_time):
 		last_time = int(elapsed_time)
-		GameEvents.emit_game_time_updated(elapsed_time)
+		
+		GameEvents.emit_game_time_updated(timer.wait_time - elapsed_time)
+#		GameEvents.emit_game_time_updated(elapsed_time)
+
+
+func add_time():
+	var new_time = timer.time_left + 5
+	timer.wait_time = new_time
+	timer.start()
+	elapsed_time = 0
 
 
 func on_timer_timeout():
--- scenes/ui/player_ui/player_ui.gd
+++ scenes/ui/player_ui/player_ui.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
 @onready var time_label = $%TimeLabel
+@onready var kill_label = $%KillLabel
 @onready var fps_counter = $%FPSCounter
+
+var num_kills = 0
 
 
 func _ready():
 	GameEvents.game_time_updated.connect(on_game_time_updated)
+	GameEvents.enemy_died.connect(func(): update_kill_label())
 	
 	var viewport: SubViewport = $SubViewport
 	viewport.set_clear_mode(SubViewport.CLEAR_MODE_ALWAYS)
@@ -16,12 +20,19 @@ func _ready():
 
 
 func _process(delta):
-	fps_counter.set_text(str(Engine.get_frames_per_second()) + " fps")
+#	fps_counter.set_text(str(Engine.get_frames_per_second()) + " fps")
+	time_label.set_text(str(Engine.get_frames_per_second()) + " fps")
+
+
+func update_kill_label():
+	num_kills += 1
+	kill_label.set_text("%02d DESTROYED" % num_kills)
 
 
 func on_game_time_updated(time_elapsed):
 	var minute: int = time_elapsed / 60
 	var second: int = time_elapsed - minute * 60
 	
-	time_label.set_text("%02d:%02d" % [minute, second])
+	fps_counter.set_text("%02d:%02d" % [minute, second])
+#	time_label.set_text("%02d:%02d" % [minute, second])
 #	print("%02d:%02d" % [minute, second])
