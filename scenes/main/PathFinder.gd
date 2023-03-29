f94a7b4730b597ede89ac40c505214ca1671d508
Sakkris <cristovaogmsampaio0@gmail.com>
2023-03-29 15:26:03 +0100

    [WARN] Implementei o pathfinder, versão muito hardcoded, deve ser melhorado futuramente.

--- scenes/main/PathFinder.gd
+++ scenes/main/PathFinder.gd
@@ -4,28 +4,47 @@ var player
 var current_line: MeshInstance3D
 var material: StandardMaterial3D
 var destiny_node
-var nav_points = []
+var nav_points = {}
+var astar: AStar3D = AStar3D.new()
 
 
 func _ready():
 	GameEvents.enemy_spawned.connect(on_enemy_spawned)
 	
 	player = get_tree().get_first_node_in_group("player")
-	nav_points = get_children()
+	
+	var nav_points_nodes = get_children()
+	var index = 0
+	
+	for nav_point in nav_points_nodes:
+		nav_points[index] = nav_point
+		index += 1
 	
 	material = StandardMaterial3D.new()
 	material.vertex_color_use_as_albedo = true
 	
-	current_line = create_array_mesh(player.position, get_nearest_nav_point_to_node(player).position)
+	setup_astar()
+	
+	var orig_id = get_nearest_nav_point_to_node(player)
+	var dest_id
+	if destiny_node == null:
+		dest_id = get_nearest_nav_point_to_node(player)
+	else:
+		dest_id = get_nearest_nav_point_to_node(destiny_node)
+	
+	current_line = create_array_mesh(orig_id, dest_id)
 
 
 func _process(delta):
-	update_array_mesh(player.position, get_nearest_nav_point_to_node(player).position)
+	var orig_id = get_nearest_nav_point_to_node(player)
+	var dest_id = get_nearest_nav_point_to_node(destiny_node)
+	
+	update_array_mesh(orig_id, dest_id)
 
 
-func update_array_mesh(pos1: Vector3, pos2: Vector3):
+func update_array_mesh(orig_id, dest_id):
 	var arr_mesh: ArrayMesh = current_line.mesh
-	var arrays = create_arrays(pos1, pos2)
+	var arrays = create_arrays(orig_id, dest_id)
 	
 	arr_mesh.clear_surfaces()
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arrays)
@@ -34,9 +53,9 @@ func update_array_mesh(pos1: Vector3, pos2: Vector3):
 	current_line.material_override = material
 
 
-func create_array_mesh(pos1: Vector3, pos2: Vector3) -> MeshInstance3D:
+func create_array_mesh(orig_id, dest_id) -> MeshInstance3D:
 	var arr_mesh = ArrayMesh.new()
-	var arrays = create_arrays(pos1, pos2)
+	var arrays = create_arrays(orig_id, dest_id)
 	
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arrays)
 	var m = MeshInstance3D.new()
@@ -49,14 +68,31 @@ func create_array_mesh(pos1: Vector3, pos2: Vector3) -> MeshInstance3D:
 	return m
 
 
-func create_arrays(pos1: Vector3, pos2: Vector3):
+func create_arrays(orig_id, dest_id):
 	var vertices = PackedVector3Array()
-	vertices.push_back(pos1)
-	vertices.push_back(pos2)
+	var prev_vertice = null
+	var id_path = astar.get_id_path(orig_id, dest_id)
+	
+	for point_id in id_path:
+		if prev_vertice != null:
+			vertices.push_back(nav_points[prev_vertice].position)
+			vertices.push_back(nav_points[point_id].position)
+		else: 
+			vertices.push_back(player.position)
+			vertices.push_back(nav_points[point_id].position)
+		
+		prev_vertice = point_id
 	
 	if destiny_node != null:
-		vertices.push_back(get_nearest_nav_point_to_node(destiny_node).position)
+		vertices.push_back(nav_points[prev_vertice].position)
 		vertices.push_back(destiny_node.position)
+	
+#	vertices.push_back(pos1)
+#	vertices.push_back(pos2)
+#
+#	if destiny_node != null:
+#		vertices.push_back(get_nearest_nav_point_to_node(destiny_node).position)
+#		vertices.push_back(destiny_node.position)
 	
 	var arrays = []
 	arrays.resize(Mesh.ARRAY_MAX)
@@ -72,19 +108,58 @@ func create_arrays(pos1: Vector3, pos2: Vector3):
 	return arrays
 
 
-func get_nearest_nav_point_to_node(node: Node3D) -> Node3D:
+func get_nearest_nav_point_to_node(node: Node3D):
 	var nearest_point
 	var nearest_distance = 999.9
 	var current_distance 
 	
 	for nav_point in nav_points:
-		current_distance = nav_point.position.distance_squared_to(node.position)
+		current_distance = nav_points[nav_point].position.distance_squared_to(node.position)
 		
 		if current_distance < nearest_distance || nearest_point == null:
 			nearest_distance = current_distance
 			nearest_point = nav_point
 	
 	return nearest_point 
+
+
+func get_connected_nav_points_to_point(point):
+	var connected_points = [null, null]
+	var nearest_distances = [999.9, 999.9]
+	var current_distance = 0
+	
+	for nav_point in nav_points:
+		if nav_point != point:
+			current_distance = nav_points[nav_point].position.distance_squared_to(nav_points[point].position)
+			
+			if current_distance < nearest_distances[0] || connected_points[0] == null:
+				nearest_distances[0] = current_distance
+				connected_points[0] = nav_point
+			elif current_distance < nearest_distances[1] || connected_points[1] == null:
+					nearest_distances[1] = current_distance
+					connected_points[1] = nav_point
+	
+	return connected_points
+
+
+func add_astar_points():
+	for nav_point in nav_points:
+		astar.add_point(nav_point, nav_points[nav_point].position)
+
+
+func make_astar_connections():
+	var connected_points
+	
+	for nav_point in nav_points:
+		connected_points = get_connected_nav_points_to_point(nav_point)
+		
+		astar.connect_points(nav_point, connected_points[0])
+		astar.connect_points(nav_point, connected_points[1])
+
+
+func setup_astar():
+	add_astar_points()
+	make_astar_connections()
 
 
 func on_enemy_spawned(enemy):
