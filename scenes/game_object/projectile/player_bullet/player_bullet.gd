270d9a2c4daa2bd06f36efff89f68ee268f69d14
Sakkris <cristovaogmsampaio0@gmail.com>
2023-04-26 15:23:57 +0100

    Adicionei Particulas ao laser do jogador quando colide com alguma coisa.

--- scenes/game_object/projectile/player_bullet/player_bullet.gd
+++ scenes/game_object/projectile/player_bullet/player_bullet.gd
@@ -1,15 +1,30 @@
 extends Area3D
 
-const BULLET_SPEED = 50.0
+@onready var hit_particles: CPUParticles3D = $HitParticles
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var bullet_speed = 50.0
 
 func _ready():
 	$BulletLifeTime.timeout.connect(self.on_lifetime_timeout)
-
+	area_entered.connect(on_area_entered)
+	body_entered.connect(on_body_entered)
 
 func _process(delta):
-	global_translate(transform.basis.z * -BULLET_SPEED * delta)
+	global_translate(transform.basis.z * -bullet_speed * delta)
 
+
+func hit_target():
+	bullet_speed = 0
+	$AnimationPlayer.play("hit")
 
 func on_lifetime_timeout():
-	queue_free()
+	hit_target()
+
+
+func on_area_entered(_other_area):
+	hit_target()
+
+
+func on_body_entered(_other_body):
+	hit_target()
