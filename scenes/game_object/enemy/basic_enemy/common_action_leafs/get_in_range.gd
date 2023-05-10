b7f84685dd337e71011c7998598cbff779f3c230
Sakkris <cristovaogmsampaio0@gmail.com>
2023-05-10 16:43:51 +0100

    Alterei as texturas do E-Type Drone para ter uma Emissão Vermelha.

--- scenes/game_object/enemy/basic_enemy/common_action_leafs/get_in_range.gd
+++ scenes/game_object/enemy/basic_enemy/common_action_leafs/get_in_range.gd
@@ -17,11 +17,11 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 	var is_player_far = current_location.distance_squared_to(player_location) >= 30 ** 2
 	
 	if !path_cooldown_timer.is_stopped() && is_player_far:
-		return SUCCESS
+		return RUNNING
 	
 	var path = NavPointGenerator.generate_path(current_location, player_location)
 	
 	if is_player_far:
 		path_cooldown_timer.start()
 	
-	return SUCCESS
+	return RUNNING
