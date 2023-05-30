318712de69286ad5d3cdac111cecdd737b78f51e
Sakkris <cristovaogmsampaio0@gmail.com>
2023-05-30 16:49:50 +0100

    Adicionei uma forma de adicionar overlays ao Jogador. Agora há um shader constante para simular a visão de um robo.

---
+++ scenes/ui/camera_overlay/camera_overlay.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+func _ready():
+	var viewport: SubViewport = $SubViewport
+	viewport.set_clear_mode(SubViewport.CLEAR_MODE_ALWAYS)
+	viewport.transparent_bg = true
+	
+	$ViewportQuad.material_override.albedo_texture = viewport.get_texture()
+	$ViewportQuad.material_override.flags_transparent = true
